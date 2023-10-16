3308ef2da10fe982d3a1974a909eca3b8f79f98e
Carey99 <edwinakidah1@gmail.com>
2023-10-16 04:04:55 -0700

    Unused files removed

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "printfc.h"
 #include "main.h"
 #include <limits.h>
 
--- printfc.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINTFC_H
-#define PRINTFC_H
-
-#include <stdarg.h>
-
-int printfc(va_list num);
-
-#endif
-
