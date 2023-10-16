e41625e2354699ff3b5f22cb94d8f1e012cbabec
Carey99 <edwinakidah1@gmail.com>
2023-10-16 11:42:51 -0700

    Theprevious commit didn't work

--- putting_s.c
+++ putting_s.c
@@ -1,32 +1,28 @@
 #include "main.h"
+#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <stdarg.h>
+#include <stdlib.h>
 
 /**
 * putting_s - Prints string
-* @s: string
-* Return: Length of the string
+* @s: String being printed
+* Return: Count number
 */
 
 int putting_s(char *s)
 {
-	int i, length;
+	int count = 0;
 
 	if (s == NULL)
 	{
-	s = "(null)";
-	length = strlen(s);
-	for (i = 0; i < length; i++)
-	putchar(s[i]);
-	return (length);
+	count += write(1, "(null)", 6);
 	}
 	else
 	{
-	length = strlen(s);
-	for (i = 0; i < length; i++)
-	putchar(s[i]);
-	return (length);
+	int len = strlen(s);
+
+	count += write(1, s, len);
 	}
+	return (count);
 }
